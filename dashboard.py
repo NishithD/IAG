4ae0d4fede3bda6f7dff140280c03bfc52db1356
NishithD <nishithdasari100@gmail.com>
2024-04-24 13:00:14 +0530

    fix

--- dashboard.py
+++ dashboard.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle
 import pandas as pd
 import numpy as np
-import plotly  
+import plotly 
 import plotly.graph_objs as go
 import plotly.figure_factory as ff
 import plotly.express as px  
